646110a27e7167a26a5b8e0f85d9751a832f7d04
--add <deshmukhswapnil316@gmail.com>
2022-07-23 12:13:58 +0530

    Starting Backend Development

--- server/router/router.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const controller = require('../controller/controller')
-
-router.post('/register',controller.registerUser)
-router.post('/login',controller.login);
-
-module.exports = router
